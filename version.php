c7b1215a0548e3a2530d5b13aa995c5584d71583
Dan Marsden <dan@danmarsden.com>
2014-09-19 20:28:10 +1200

    Update copyright/boilerplate text.

--- version.php
+++ version.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -16,6 +15,10 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * Version information
+ *
+ * @package    report_coursesize
+ * @copyright  2014 Catalyst IT {@link http://www.catalyst.net.nz}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
