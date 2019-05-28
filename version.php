c9f2b9dea10c42e9bdb2433f3b9b5bc17b3b1551
Dan Marsden <dan@danmarsden.com>
2019-05-28 13:31:52 +1200

    Bump version for plugins db release.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2019051500;
+$plugin->version  = 2019052800;
 $plugin->requires = 2018120300; // Requires 3.6.
 $plugin->component = 'report_coursesize';
 $plugin->release = '3.6';
